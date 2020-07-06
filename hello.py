9d808e43b05b2444c7c541c8ba1e0be3eb5f0e21
Pritesh Kakani <priteshgkakani@gmail.com>
2020-07-06 12:33:11 +0530

    URL Building url_for

--- hello.py
+++ hello.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Index Page'
 
+@app.route('/login')
+def login():
+    return 'login'
+
 @app.route('/hello')
 def hello():
     return 'Hello, World'
 
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
 @app.route('/projects/')
 def projects():
     return 'The project page'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
